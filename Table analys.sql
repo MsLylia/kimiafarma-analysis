7e4205ac34e2d67c16e7968cf4cb97b4c3e5fd43
Lia Kilmas <89234929+MsLylia@users.noreply.github.com>
2025-03-31 17:54:14 +0700

    create tablet analys
    create tablet analys

---
+++ Table analys.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE TABLE `soy-tube-454913-n2.kimia_farma.kf_analisis` AS  
+SELECT  
+    t.transaction_id,  
+    t.date,  
+    c.branch_id,  
+    c.branch_name,  
+    c.kota,  
+    c.provinsi,  
+    c.rating AS rating_cabang,  
+    t.customer_name,  
+    p.product_id,  
+    p.product_name,  
+    t.price AS actual_price,  
+    t.discount_percentage,  
+    (t.price - (t.price * t.discount_percentage / 100)) AS nett_sales,  
+    CASE  
+        WHEN t.price <= 50000 THEN 0.10  
+        WHEN t.price > 50000 AND t.price <= 100000 THEN 0.15  
+        WHEN t.price > 100000 AND t.price <= 300000 THEN 0.20  
+        WHEN t.price > 300000 AND t.price <= 500000 THEN 0.25  
+        ELSE 0.30  
+    END AS persentase_gross_laba,  
+    ((t.price - (t.price * t.discount_percentage / 100)) *  
+        CASE  
+            WHEN t.price <= 50000 THEN 0.10  
+            WHEN t.price > 50000 AND t.price <= 100000 THEN 0.15  
+            WHEN t.price > 100000 AND t.price <= 300000 THEN 0.20  
+            WHEN t.price > 300000 AND t.price <= 500000 THEN 0.25  
+            ELSE 0.30  
+        END) AS nett_profit,  
+    t.rating AS rating_transaksi  
+FROM `soy-tube-454913-n2.kimia_farma.kf_final_transaction` t  
+JOIN `soy-tube-454913-n2.kimia_farma.kf_product` p ON t.product_id = p.product_id  
+JOIN `soy-tube-454913-n2.kimia_farma.kf_kantor_cabang` c ON t.branch_id = c.branch_id;
+
+SELECT * FROM `soy-tube-454913-n2.kimia_farma.kf_analisis`  LIMIT 10;
+
+
